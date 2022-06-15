eb11fbcd7e4a39a9b98f0d442dd73f6e6e23ab6a
bobboec <30457666+bobboec@users.noreply.github.com>
2022-06-15 22:19:32 +0300

    debug adaptiv

--- src/components/NotFoundBlock/index.jsx
+++ src/components/NotFoundBlock/index.jsx
@@ -6,13 +6,15 @@ console.log(styles);
 
 const NotFoundBlock = () => {
   return (
-    <div className={styles.root}>
-      <h1>
-        <span>&#128514;</span>
-        <br />
-        Ничего не найдено :(
-      </h1>
-      <p className={styles.description}>К сожалению данная страница отсутсвует на нашем сайте</p>
+    <div className="container">
+      <div className={styles.root}>
+        <h1>
+          <span>&#128514;</span>
+          <br />
+          Ничего не найдено :(
+        </h1>
+        <p className={styles.description}>К сожалению данная страница отсутсвует на нашем сайте</p>
+      </div>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,20 +16,23 @@ function Home() {
         setItems(arr);
         setIsLoading(false);
       });
+    window.scrollTo(0, 0);
   }, []);
 
   return (
     <>
-      <div className="content__top">
-        <Categories />
-        <Sort />
-      </div>
-      <h2 className="content__title"> Все пиццы </h2>
-      <div className="content__items">
-        {/* {items.map((obj) => (isLoading ? <Skeleton /> : <PizzaBlock key={obj.id} {...obj} />))} */}
-        {isLoading
-          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      <div className="container">
+        <div className="content__top">
+          <Categories />
+          <Sort />
+        </div>
+        <h2 className="content__title"> Все пиццы </h2>
+        <div className="content__items">
+          {/* {items.map((obj) => (isLoading ? <Skeleton /> : <PizzaBlock key={obj.id} {...obj} />))} */}
+          {isLoading
+            ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+            : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+        </div>
       </div>
     </>
   );
