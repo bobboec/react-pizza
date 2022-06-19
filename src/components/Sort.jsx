7c2e5749d374da610e8e336420ef565f437fa79f
bobboec <30457666+bobboec@users.noreply.github.com>
2022-06-19 23:52:19 +0300

    sorting by category

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,43 +1,45 @@
 import React from 'react';
 
-function Sort() {
+function Sort({ sortValue, onClickSortType }) {
   const [isVisible, setIsVisible] = React.useState(false);
-  const [selectedItem, setSelectedItem] = React.useState(0);
-  const sortList = ['популярности', 'цене', 'алфавиту'];
-  const selectItem = sortList[selectedItem];
+  const sortList = [
+    { name: 'популярности', sortProperty: 'rating' },
+    { name: 'цене', sortProperty: 'price' },
+    { name: 'алфавиту', sortProperty: 'title' },
+  ];
   const selectListItem = (index) => {
-    setSelectedItem(index);
+    onClickSortType(index);
     setIsVisible(false);
   };
-
   return (
     <div className="sort">
       <div className="sort__label">
         <svg
-          width="10"
-          height="6"
-          viewBox="0 0 10 6"
+          width="12"
+          height="8"
+          viewBox="0 0 12 8"
           fill="none"
           xmlns="http://www.w3.org/2000/svg">
           <path
             d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
             fill="#2C2C2C"
-            transform-origin="center center"
-            transform={isVisible ? undefined : 'rotate(180)'}
+
+            // transform-origin="center center"
+            // transform={isVisible ? undefined : 'rotate(180)'}
           />
         </svg>
         <b>Сортировка по:</b>
-        <span onClick={() => setIsVisible(!isVisible)}>{selectItem}</span>
+        <span onClick={() => setIsVisible(!isVisible)}>{sortValue.name}</span>
       </div>
       {isVisible && (
         <div className="sort__popup">
           <ul>
-            {sortList.map((value, index) => (
+            {sortList.map((obj, index) => (
               <li
                 key={index}
-                onClick={() => selectListItem(index)}
-                className={selectedItem === index ? 'active' : ''}>
-                {value}
+                onClick={() => selectListItem(obj)}
+                className={sortValue.sortProperty === obj.sortProperty ? 'active' : ''}>
+                {obj.name}
               </li>
             ))}
           </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,23 +8,41 @@ import Skeleton from '../components/PizzaBlocks/Skeleton';
 function Home() {
   let [items, setItems] = React.useState([]);
   let [isLoading, setIsLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [sortType, setSortType] = React.useState({
+    name: 'популярности',
+    sortProperty: 'rating',
+  });
+  const [sortWay, setSortWay] = React.useState(false);
 
+  console.log(sortType);
+  //https://629c7c523798759975d66627.mockapi.io/items?category=2&sortBy=price&order=asc
   React.useEffect(() => {
-    fetch('https://629c7c523798759975d66627.mockapi.io/items')
+    setIsLoading(true);
+    fetch(
+      `https://629c7c523798759975d66627.mockapi.io/items?${
+        categoryId > 0 ? `category=${categoryId}` : ''
+      }&sortBy=${sortType.sortProperty}&order=${sortWay ? 'asc' : 'desc'}`,
+    )
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, []);
+  }, [categoryId, sortType, sortWay]);
 
   return (
     <>
       <div className="container">
         <div className="content__top">
-          <Categories />
-          <Sort />
+          <Categories value={categoryId} onClickCat={(id) => setCategoryId(id)} />
+          <Sort
+            sortValue={sortType}
+            // sortWay={sortWay}
+            onClickSortType={(sortProperty) => setSortType(sortProperty)}
+            // onClickSortWay={(way) => setSortWay(way)}
+          />
         </div>
         <h2 className="content__title"> Все пиццы </h2>
         <div className="content__items">
